17db0b34d01865bdb81ea7a24d9a499736aa014c
Bashyan <rgbine@gmail.com>
2016-11-08 17:58:06 +0530

    Add files via upload

---
+++ exceptionHandling.java
@@ -0,0 +1,35 @@
+public class exceptionHandling
+{
+	static void handling()
+		{
+		int a = 3;
+		int b = 0;
+		try
+			{
+			b = a/b;
+			}
+		catch (ArithmeticException e)
+			{
+			System.out.println(b);
+			}
+		finally
+			{
+			System.out.println("error");
+			}
+		System.out.println("end");
+		}
+
+	static void array()
+		{
+		String a1[] = {"a","b","c","d","e","f"};
+		for(int i = 0; i<a1.length+1;i++)
+			{
+			System.out.print(a1);
+			}
+		}
+	public static void main(String str[])
+		{
+		handling();
+		array();
+		}
+}
